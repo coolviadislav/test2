db2a8f700b655846dade939684e94177ae7a093c
coolviadislav <73261367+coolviadislav@users.noreply.github.com>
2020-11-12 16:29:48 +0300

    Add files via upload

---
+++ parserOlimp.php
@@ -0,0 +1,14 @@
+<?php
+	include('lib/simple_html_dom.php');
+	
+	$html = file_get_html("https://olimp-cars.ru/auto");
+	$cars = [];
+	foreach ($html->find('.item') as $key => $value) {
+		$cars[] = [
+			"name" =>trim($html->find('.item_name',$key)->plaintext),
+			"price" =>intval(preg_replace("/[^,.0-9]/", '', $html->find('.item_new_price ',$key)->plaintext)),
+			"img" =>$html->find('.item_image img',$key)->src
+		];
+	}
+	echo json_encode($cars);
+
